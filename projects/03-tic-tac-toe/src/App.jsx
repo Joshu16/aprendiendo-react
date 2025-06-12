4ddbcf032985d7f4f79ce8b696cbb89016c77a0e
Joshua <jsaborio1604@gmail.com>
2025-06-12 13:45:16 -0600

    Cambios CSS

--- projects/03-tic-tac-toe/src/App.jsx
+++ projects/03-tic-tac-toe/src/App.jsx
@@ -1,41 +1,37 @@
 import React, { useState } from "react"; // Asegúrate de importar useState
 import confetti from "canvas-confetti";
-import  { Square } from "./components/square.jsx"
-import { TURNS, WINNER_COMBOS} from "./constants.js"
-
-
+import { Square } from "./components/square.jsx";
+import { TURNS, WINNER_COMBOS } from "./constants.js";
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
 
- 
-
   const [turn, setTurn] = useState(TURNS.X);
-  const [winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState(null);
 
   const checkWinner = (boardToCheck) => {
     for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo
+      const [a, b, c] = combo;
       if (
         boardToCheck[a] &&
         boardToCheck[a] === boardToCheck[b] &&
         boardToCheck[a] === boardToCheck[c]
       ) {
-        return boardToCheck[a]
+        return boardToCheck[a];
       }
     }
-    return null
-  }
+    return null;
+  };
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
-  }
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
 
   const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
+    return newBoard.every((square) => square !== null);
+  };
 
   // Actualiza el tablero en la posición clickeada
   const updateBoard = (index) => {
@@ -50,27 +46,23 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
     //Revisar si hay ganador
-    const newWinner =checkWinner(newBoard)
+    const newWinner = checkWinner(newBoard);
     if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-    } else if (checkEndGame(newBoard)) { // check if game is over
-      setWinner(false)
+      confetti();
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      // check if game is over
+      setWinner(false);
     }
-  }
+  };
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
-      <button onClick={resetGame}>Reset del Juego</button>
       <section className="game">
         {board.map((square, index) => {
           return (
-            <Square
-              key={index}
-              index={index}
-              updateBoard={updateBoard}
-            >
+            <Square key={index} index={index} updateBoard={updateBoard}>
               {square}
             </Square>
           );
@@ -81,30 +73,23 @@ function App() {
         <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      {
-        winner !== null && (
-          <section className="winner">
-            <div className="text">
-              <h2>
-                {
-                  winner === false
-                  ? 'Empate'
-                  : 'Gano'
-                }
-              </h2>
 
-              <header className="win">
-                {winner && <Square>{winner}</Square>}
+      <button onClick={resetGame}>Reset del Juego</button>
 
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Empate" : "Ganó"}</h2>
 
-              </header>
-              <footer>
-                <button onClick={resetGame}>Empezar de nuevo</button>
-              </footer>
-            </div>
-          </section>
-        )
-      }
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
